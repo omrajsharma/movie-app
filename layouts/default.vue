5766a362e177c10ff112d9f393552f482ab4ca24
omrajsharma <omraj7413@gmail.com>
2022-02-05 20:51:07 +0530

    first commit - Movie listing on page done

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="app">
+    <!-- Omraj Sharma Development -->
+    <Nuxt/>
+</div>
+</template>
+
+<style scoped>
+.app {
+    color: white;
+}
+</style>
---
+++ pages/movie/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        All Movies
+    </div>
+</template>
